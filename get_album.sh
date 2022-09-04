343f87b482c21ace8f78ec6d69f50bd35754e01a
Neil Walker <neil@wynded.co.uk>
2022-09-04 15:18:11 +0100

    Minor tidying / Support info level

--- get_album.sh
+++ get_album.sh
@@ -5,15 +5,15 @@ source "$(dirname $0)/lib_piju_client.sh"
 album_id=$1
 if [ -z "$album_id" ]; then
   echo "Usage: $0 ALBUMID [TRACKINFOLEVEL]"
-  echo "TRACKINFOLEVEL can be one of 'none', 'all' or 'links' (the default)"
+  echo "TRACKINFOLEVEL can be one of 'none', 'links' (the default), 'all' or 'debug'"
   exit 1
 fi
 
 trackinfolevel=$2
 set -e
 set -o pipefail
-if [ -z "$trackinfolevel" ]; then
-  curl -sf --show-error "$PIJU_BASE_URL/albums/$album_id" | jq
-else
-  curl -sf --show-error "$PIJU_BASE_URL/albums/$album_id?tracks=$trackinfolevel" | jq
+if [ "$trackinfolevel" ]; then
+  trackinfolevel="?tracks=$trackinfolevel"
 fi
+
+curl -sf --show-error "$PIJU_BASE_URL/albums/$album_id$trackinfolevel" | jq
--- get_track.sh
+++ get_track.sh
@@ -2,10 +2,20 @@
 
 source "$(dirname $0)/lib_piju_client.sh"
 
+set -e
+set -o pipefail
+
 trackid=$1
 if [ -z "$trackid" ]; then
   echo Usage: $0 TRACKID
   exit 1
 fi
 
-curl -sf "$PIJU_BASE_URL/tracks/$trackid" | jq
+infolevel=$2
+
+if [ "$infolevel" ]; then
+  infolevel="?infolevel=$infolevel"
+fi
+
+
+curl -sf "$PIJU_BASE_URL/tracks/$trackid$infolevel" | jq
