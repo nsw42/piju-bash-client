81d81cd4c613dc864b429f73ac5e8ead2cfb68ac
Neil Walker <neil@wynded.co.uk>
2022-09-04 15:17:32 +0100

    Allow starting track to be specified

--- play_album.sh
+++ play_album.sh
@@ -2,6 +2,14 @@
 source lib_piju_client.sh
 
 albumid=$1
-# TODO: Check $albumid is numeric
+trackid=$2
+
+if [ "$trackid" ]; then
+  request='{"album": '$albumid', "track": '$trackid'}'
+else
+  request='{"album": '$albumid'}'
+fi
+
+# TODO: Check $albumid and $trackid are numeric
 set -x
-curl -X POST "${CONTENT_TYPE[@]}" -d '{"album": '$albumid'}' "$PIJU_BASE_URL/player/play"
+curl -X POST "${CONTENT_TYPE[@]}" -d "$request" "$PIJU_BASE_URL/player/play"
